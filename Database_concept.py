697df78697b31bff78d459a938ba36e78450a351
Dave-B <insathius@gmail.com>
2022-08-15 10:36:59 +0100

    Removed the .env and .gitignore logic as panic suggested, due to redeem needing to have the same name in order to be selected.
    Removed rangecomprehension as range already is indexed at 0 as arrays are.
    
    Reviewed notes and removed test and concept based ones to make code more clear.
    
    Added logic to create a .env file if it doesn't exist with syntax for keys.
    
    Reviewed output and made them more user friendly.

--- Database_concept.py
+++ Database_concept.py
@@ -9,20 +9,29 @@ import keyboard
 from decouple import config
 
 def callback_redemptions(uuid: UUID, data: dict) -> None:
-    #grabbing title
+    # Grabbing title
     redeemed = data["data"]["redemption"]["reward"]["title"]
     redeemed += '*'
-    #Build the  DB
+    # Build the  DB
     db = TinyDB('db.json', encoding = "utf8")
     Punished_Sounds = db.table('FILES')
     File = Query()
-    #get the working dir
+    # Get the working dir
     cwd = os.getcwd()
     sounds = os.listdir(cwd)
-    if 'config.env' in sounds:
-        sounds.remove('config.env')
-    if '.gitignore' in sounds:
-        sounds.remove('.gitignore')
+    # Checking for .env file, create if not there
+    print("Checking for .env settings file")
+    if sounds = '.env':
+        time.sleep(1)
+        print(".env file detected")
+    else:
+        with open('.env', 'w') as f:
+            f.write('APP_ID = \n')
+            f.write('SECRET = \n')
+            f.write('CHANNEL_ID = \n')
+        time.sleep(1)
+        print(".env file created, please fill in keys")
+        exit()
     print("Checking database exists...")
     if len(Punished_Sounds) == 0:
         for x in sounds:
@@ -31,8 +40,10 @@ def callback_redemptions(uuid: UUID, data: dict) -> None:
             'Playcount': int(1)
             })
         print(f"Inserted {len(Punished_Sounds)} rows into your database")
-    time.sleep(2)
-    print("Check completed, thanks for waiting" "\n")
+        time.sleep(2)
+        print("Please restart application to use your new database" "\n")
+        exit()
+    # Database refresh function, adding only differences in files
     def  database_check():
         item = Punished_Sounds.all()
         file = [row['File'] for row in item]
@@ -42,67 +53,62 @@ def callback_redemptions(uuid: UUID, data: dict) -> None:
             'File': sound,
             'Playcount': int(1)
             })
-    #Searching for all files
+    # Searching for all files
     sound_search = Punished_Sounds.search(File.File.search(redeemed))
-    #Getting length
+    # Getting length to check if random or not
     range = len(sound_search)
-    #Since lists start at index 0, need to add 0 and remove one from length
-    rangecomprehension = [0]
-    rangecomprehension.append(range - 1)
     if range == 1:
-        print("Not Random")
         searchpop = sound_search.pop(0)
         filepop = searchpop.get("File")
-        #Playsound here
-        print(filepop)
-        #Increasing playcount
+        # Playsound here
+        print(f"Redeemed non-random sound played {filepop}""\n")
+        playsound(filepop)
+        # Increasing playcount
         playcountpop = int(searchpop.get("Playcount"))
         newplay = playcountpop +1
         Punished_Sounds.upsert({'Playcount': newplay}, File.File == filepop)
     else:
-        print("Random")
-        # Temp table idea - need to drop this after
+        # Temp table of search results
         Search_Results = db.table('SEARCH_RESULTS')
         for results in sound_search:
             Search_Results.insert({
             'File': results.get("File"),
             'Playcount': results.get("Playcount")
             })
-            #Logic to get the least played from temp table
+            # Logic to get the least played from temp table
             all_playcount = Search_Results.all()
             playcount = {row['Playcount'] for row in all_playcount}
             minplay = min(playcount)
             least_played = Search_Results.search(File.Playcount == minplay)
-            #Setting random here, in case playcount matches multiple files
+            # Setting random here, in case playcount matches multiple files
             randomfile = random.sample(least_played, 1)
             filepop = randomfile.pop(0)
             thefile = filepop.get("File")
-            #Playsound here
-            print(thefile)
-            #Drop temp table
+            # Playsound here
+            print(f"Redeemed random sound played {thefile}"\n")
+            playsound(thefile)
+            # Drop temp table
             db.drop_table('SEARCH_RESULTS')
-            #Increasing playcount
+            # Increasing playcount
             newplay = minplay +1
             Punished_Sounds.upsert({'Playcount': newplay}, File.File == thefile)
-
+# Detecting R key and doing refresh if pressed
     if keyboard.is_pressed("r"):
         database_check()
         print("Database refresh completed")
 # setting up Authentication and getting your user id
 twitch = Twitch(config('APP_ID'), config('SECRET'))
 twitch.authenticate_app([])
-# you can get your user auth token and user auth refresh token following the example in twitchAPI.oauth
-target_scope = [AuthScope.CHANNEL_READ_REDEMPTIONS]
+target_scope = [AuthScope.CHANNEL_R0EAD_REDEMPTIONS]000
 auth = UserAuthenticator(twitch, target_scope, force_verify=False)
 token, refresh_token = auth.authenticate()
 twitch.set_user_authentication(token, target_scope, refresh_token)
 channel_id = config('CHANNEL_ID')
-# starting up PubSub
+# Starting up PubSub
 pubsub = PubSub(twitch)
 pubsub.start()
-# you can either start listening before or after you started pubsub.
 uuid = pubsub.listen_channel_points(channel_id, callback_redemptions)
-input('You can press ENTER in order to close script')
-# you do not need to unlisten to topics before stopping but you can listen and unlisten at any moment you want
-pubsub.unlisten(uuid)
-pubsub.stop()
+# Below logic to stop script if input, removed for database_check
+#input('You can press ENTER in order to close script')
+#pubsub.unlisten(uuid)
+#pubsub.stop()
